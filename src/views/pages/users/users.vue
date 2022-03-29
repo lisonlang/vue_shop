419744300bb7a50f5cb9331c94473814f3d3fe11
lisonlang <1092037197@qq.com>
2022-03-29 21:35:13 +0800

    用户管理删除与修改

--- src/views/pages/users/users.vue
+++ src/views/pages/users/users.vue
@@ -41,19 +41,18 @@
           </el-table-column>
           <!-- 表格按钮 -->
           <el-table-column align="center" label="操作" header-align="left">
-            <!-- 暂时先这样写着 -->
             <template slot-scope="scope">
               <el-button
                 type="primary"
                 icon="el-icon-edit"
-                @click="dialogFormVisible = true"
+                @click="edituser(scope.row)"
                 size="small"
               ></el-button>
               <!-- 删除 -->
               <el-button
                 type="danger"
                 icon="el-icon-delete"
-                @click="delinfo = true"
+                @click="deluser(scope.row.id)"
                 size="small"
               ></el-button>
               <el-button
@@ -85,20 +84,18 @@
     <el-dialog title="修改用户" :visible.sync="dialogFormVisible">
       <el-form :model="form">
         <el-form-item label="用户名" :label-width="formLabelWidth">
-          <el-input autocomplete="off"></el-input>
+          <el-input autocomplete="off" v-model="form.name"></el-input>
         </el-form-item>
         <el-form-item label="邮箱" :label-width="formLabelWidth">
-          <el-input autocomplete="off"></el-input>
+          <el-input autocomplete="off" v-model="form.email"></el-input>
         </el-form-item>
         <el-form-item label="手机" :label-width="formLabelWidth">
-          <el-input autocomplete="off"></el-input>
+          <el-input autocomplete="off" v-model="form.tel"></el-input>
         </el-form-item>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
-        <el-button type="primary" @click="dialogFormVisible = false"
-          >确 定</el-button
-        >
+        <el-button type="primary" @click="sureEdit">确 定</el-button>
       </div>
     </el-dialog>
     <!-- 删除信息的对话框 -->
@@ -106,9 +103,7 @@
       <span>此操作将永久删除该文件, 是否继续?</span>
       <span slot="footer" class="dialog-footer">
         <el-button @click="delinfo = false">取 消</el-button>
-        <el-button type="primary" @click="dialogVisible = false"
-          >确 定</el-button
-        >
+        <el-button type="primary" @click="truedel">确 定</el-button>
       </span>
     </el-dialog>
     <!-- 分配角色的对话框 -->
@@ -191,7 +186,68 @@ export default {
 
       this.getuserinfo();
     },
-    // 搜索用户的方法
+    //确定修改用户的方法
+    sureEdit() {
+      this.dialogFormVisible = false;
+      request({
+        url: `/users/${this.form.id}`,
+        method: "put",
+        data: {
+          email: this.form.email,
+          mobile: this.form.tel,
+        },
+      })
+        .then((res) => {
+          if (res.data.meta.status === 200) {
+            console.log("进来了吗");
+            console.log(res.data);
+            this.getuserinfo();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    // 编辑用户框回显的方法
+    edituser(value) {
+      this.dialogFormVisible = true;
+      console.log(value);
+      this.form.id = value.id;
+      this.form.email = value.email;
+      this.form.tel = value.mobile;
+      this.form.name = value.username;
+    },
+    // 删除用户的方法
+    deluser(id) {
+      this.delinfo = true;
+      this.id = id;
+    },
+    truedel() {
+      this.delinfo = false;
+      request({
+        url: `/users/${this.id}`,
+
+        method: "delete",
+      })
+        .then((res) => {
+          if (res.data.meta.status === 200) {
+            this.$message({
+              message: "恭喜你，删除成功消息",
+              type: "success",
+            });
+            if (this.tableData.length === 1) {
+              --this.currentPage4;
+            }
+            if (this.currentPage4 < 1) {
+              this.currentPage4 = 1;
+            }
+            this.getuserinfo();
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
   data() {
     return {
@@ -220,6 +276,9 @@ export default {
           label: "北京烤鸭",
         },
       ],
+      // 要删除用户的id
+      id: "",
+
       value: "",
       // 弹出框label的宽度
       formLabelWidth: "80px",
@@ -235,6 +294,13 @@ export default {
       tableData: [],
       // 总记录数
       total: 0,
+      // 修改表单的值
+      form: {
+        id: "",
+        email: "",
+        tel: "",
+        name: "",
+      },
     };
   },
 };
